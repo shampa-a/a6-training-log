218f1ce4b59b3d01c64e746a29f74259fb5f271d
shampa-a <oofice.email2050@gmail.com>
2026-09-24 20:26:07 +0600

    The Library Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Banner from "@/components/HomePage/Banner";
+import Library from "@/components/HomePage/Library";
 
 const HomePage = () => {
   return (
-    <div>
+    <main className="bg-[#171717]">
       <Banner />
-    </div>
+      <Library />
+    </main>
   );
 };
 
